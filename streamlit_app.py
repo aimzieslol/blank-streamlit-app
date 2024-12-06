2c15f3ba776d64fd2fb585e79a40a153f9d3495b
aimzieslol <afrederico@gmail.com>
2024-12-06 14:29:16 +0000

    Changed font to Inter.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,19 @@
 import streamlit as st
 
+# Define your custom CSS
+custom_css = """
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@400&display=swap');
+
+body {
+    font-family: 'Inter'
+}
+</style>
+"""
+
+# Inject custom CSS with markdown
+st.markdown(custom_css, unsafe_allow_html=True)
+
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
